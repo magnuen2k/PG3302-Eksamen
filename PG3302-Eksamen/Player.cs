30433dcc39f49859f02013b0e1eadbe3926580e7
magnuen2k <maen030@student.kristiania.no>
2020-10-29 17:17:27 +0100

    simple add card to player functionality

---
+++ PG3302-Eksamen/Player.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PG3302_Eksamen
+{
+    public class Player
+    {
+       
+        public string Name { get; set; }
+
+        public ArrayList _hand = new ArrayList();
+
+        public ArrayList getHand()
+        {
+            return _hand;
+        }
+
+        public void setHand(Card card)
+        {
+            _hand.Add(card);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder hand = new StringBuilder();
+            
+            for (int i = 0; i < _hand.Count; i++)
+            {
+                hand.Append(_hand[i]);
+                
+                if (i < (_hand.Count - 1))
+                {
+                    hand.Append(", ");
+                }
+            }
+            return Name + " has hand: " + hand;
+        }
+    }
+}
--- PG3302-Eksamen/Program.cs
+++ PG3302-Eksamen/Program.cs
@@ -10,7 +10,17 @@ namespace PG3302_Eksamen
             card.Suit = Suit.Clubs;
             card.Value = Value.King;
             
+            Card card2 = new Card();
+            card2.Suit = Suit.Diamonds;
+            card2.Value = Value.Queen;
+            
+            Player player1 = new Player();
+            player1.Name = "Yionk";
+            player1.setHand(card);
+            player1.setHand(card2);
+            
             Console.WriteLine(card.ToString());
+            Console.WriteLine(player1.ToString());
         }
     }
 }
