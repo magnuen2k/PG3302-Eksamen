f4a6a583f54e96012da72d52bda31bcfbbffa0dc
magnuen2k <maen030@student.kristiania.no>
2020-11-15 17:14:27 +0100

    added unit tests

---
+++ PG3302-Eksamen-UnitTesting/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace PG3302_Eksamen_UnitTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
