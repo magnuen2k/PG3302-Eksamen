0d9608db0cb0e772b3c5d3bc96fd12fba9b60ab4
Kaikka <kaileeto@gmail.com>
2020-11-15 02:35:08 +0100

    extracted special cards handling from HandleCard.cs to HandleSpecialCard.cs

--- PG3302-Eksamen/HandleCard.cs
+++ PG3302-Eksamen/HandleCard.cs
@@ -10,13 +10,13 @@ namespace PG3302_Eksamen
             switch (card.GetCardType())
             {
                 case CardType.Bomb:
-                    HandleBomb(player);
+                    HandleSpecialCard.Bomb(player);
                     break;
                 case CardType.Quarantine:
-                    HandleQuarantine(player, card);
+                    HandleSpecialCard.Quarantine(player, card);
                     break;
                 case CardType.Vulture:
-                    HandleVulture(player, card);
+                    HandleSpecialCard.Vulture(player, card);
                     break;
                 case CardType.Joker:
                     break;
@@ -25,65 +25,6 @@ namespace PG3302_Eksamen
                 default:
                     throw new NotImplementedException("You drew a card with a type that cannot be handled. Code needs review.");
             }
-        }
-        
-         private static void HandleBomb(Player player)
-        {
-            Dealer dealer = Dealer.GetDealer();
-            
-            // TODO extract different operations to own functions (SRP)
-            
-            Console.WriteLine(player.Name + " has to throw away all his cards :(");
-            for (int i = player.Hand.Count() - 1; i >= 0; i--)
-            {
-                ICard c = player.Hand.GetHand()[i];
-                dealer.ReturnCard(c);
-                player.Hand.RemoveCard(c);
-                Console.WriteLine(player.Name + " returned card: " + c);
-            }
-
-            Console.WriteLine(player.Name + " draws a new hand...");
-                            
-            // draws amount of new cards depending if 
-            for (int i = 0; i < player.Hand.MaxHandSize; i++)
-            {
-                dealer.DrawNormalCard(player);
-            }
-        }
-
-        private static void HandleQuarantine(Player player, ICard card)
-        {
-            Dealer dealer = Dealer.GetDealer();
-            player.IsQuarantined = true;
-            dealer.ReturnCard(card);
-            player.Hand.RemoveCard(card);
-            Console.WriteLine(player.Name + " is now quarantined!");
-            Console.WriteLine(player.Name + " returned card: " + card +"\n");
-            Game.ShouldWeContinueTheLoop = true;
-            dealer.CloseAccess();
-        }
-
-        private static void HandleVulture(Player player, ICard card)
-        {
-            Dealer dealer = Dealer.GetDealer();
-            Console.WriteLine("Laying spider mines...");
-            dealer.DrawNormalCard(player);
-            player.Hand.RemoveCard(card); // we gain another card so our hand size is incremented by 1. Vulture effect is present by not removing a card, but we dont want to count the suit from it
-            //dealer.ReturnCard(card); // TODO - allow vulture to go back in deck? but then it needs to be in random spot, or shuffle?
-            player.Hand.MaxHandSize++;
-            Console.WriteLine("New max hand size: " + player.Hand.MaxHandSize + "\n");
-            Game.ShouldWeContinueTheLoop = true;
-            dealer.CloseAccess(); 
-        }
-
-        private static void HandleJoker(Player player, ICard card)
-        {
-            Console.WriteLine("Handling a Joker card - not yet extracted to Handle");
-        }
-
-        private static void HandleNormalCard(Player player, ICard card)
-        {
-            Console.WriteLine("Handling a normal card - not yet extracted to Handle");
         }
     }
 }
---
+++ PG3302-Eksamen/HandleSpecialCard.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace PG3302_Eksamen
+{
+    public static class HandleSpecialCard
+    {
+        public static void Bomb(Player player)
+        {
+            Dealer dealer = Dealer.GetDealer();
+            
+            // TODO extract different operations to own functions (SRP)
+            
+            Console.WriteLine(player.Name + " has to throw away all his cards :(");
+            for (int i = player.Hand.Count() - 1; i >= 0; i--)
+            {
+                ICard c = player.Hand.GetHand()[i];
+                dealer.ReturnCard(c);
+                player.Hand.RemoveCard(c);
+                Console.WriteLine(player.Name + " returned card: " + c);
+            }
+
+            Console.WriteLine(player.Name + " draws a new hand...");
+                            
+            // draws amount of new cards depending if 
+            for (int i = 0; i < player.Hand.MaxHandSize; i++)
+            {
+                dealer.DrawNormalCard(player);
+            }
+        }
+
+        public static void Quarantine(Player player, ICard card)
+        {
+            Dealer dealer = Dealer.GetDealer();
+            player.IsQuarantined = true;
+            dealer.ReturnCard(card);
+            player.Hand.RemoveCard(card);
+            Console.WriteLine(player.Name + " is now quarantined!");
+            Console.WriteLine(player.Name + " returned card: " + card +"\n");
+            Game.ShouldWeContinueTheLoop = true;
+            dealer.CloseAccess();
+        }
+
+        public static void Vulture(Player player, ICard card)
+        {
+            Dealer dealer = Dealer.GetDealer();
+            Console.WriteLine("Laying spider mines...");
+            dealer.DrawNormalCard(player);
+            player.Hand.RemoveCard(card); // we gain another card so our hand size is incremented by 1. Vulture effect is present by not removing a card, but we dont want to count the suit from it
+            //dealer.ReturnCard(card); // TODO - allow vulture to go back in deck? but then it needs to be in random spot, or shuffle?
+            player.Hand.MaxHandSize++;
+            Console.WriteLine("New max hand size: " + player.Hand.MaxHandSize + "\n");
+            Game.ShouldWeContinueTheLoop = true;
+            dealer.CloseAccess(); 
+        }
+    }
+}
